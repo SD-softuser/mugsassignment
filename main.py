0813c4abd2618d07082d2887894aad93bf49cc7f
SD-softuser <sayogdmahajan@gmail.com>
2023-11-05 19:17:32 +0530

    update endpoint done

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ async def signup(request: Request):
    email = req['email']
    password = req['password']
    username = req['username'] 
-   fullname = req['fullname']   
+   fullname = req['fullname']  
+   data = {'username': username, 'fullname': fullname, 'email':email, 'createdDtm':firestore.SERVER_TIMESTAMP} 
    if email is None or password is None:
        return HTTPException(detail={'message': 'Error! Missing Email or Password'}, status_code=400)
    try:
@@ -39,12 +40,7 @@ async def signup(request: Request):
            email=email,
            password=password
        )
-       doc_ref = db.collection("users").document(user.uid) 
-       doc_ref.set({
-        username : username,
-        fullname : fullname,
-        email : user.email,
-       })
+       db.collection("users").document(user.uid).set(data)
        return JSONResponse(content={'message': f'Successfully created user {user.uid}'}, status_code=200)    
    except:
        return HTTPException(detail={'message': 'Error Creating User'}, status_code=400)
@@ -61,7 +57,43 @@ async def login(request: Request):
        return JSONResponse(content={'token': jwt}, status_code=200)
    except:
        return HTTPException(detail={'message': 'There was an error logging in'}, status_code=400)
- 
+
+# update endpoint
+@app.put("/update", include_in_schema=False)
+async def update(request: Request):
+    req_json = await request.json()
+    jwt = req_json.get('token')  # Get the JWT token from the request
+    new_email = req_json.get('new_email')
+    new_username = req_json.get('new_username')
+    new_fullname = req_json.get('new_fullname')
+    
+    try:
+        user = auth.verify_id_token(jwt)
+        uid = user['uid']
+
+
+        if new_email:
+            auth.update_user(uid, email=new_email)
+            db.collection("users").document(uid).update({'email':new_email})
+
+
+        if new_username:
+            db.collection("users").document(uid).update({'username':new_username})
+
+        if new_fullname:
+            db.collection("users").document(uid).update({'fullname':new_fullname})
+
+        return JSONResponse(content={'message': 'User updated successfully'}, status_code=200)
+    except Exception as e:
+        error_message = str(e)
+        if 'USER_NOT_FOUND' in error_message:
+            return HTTPException(detail={'message': 'User not found'}, status_code=404)
+        elif 'INVALID_PASSWORD' in error_message:
+            return HTTPException(detail={'message': 'Invalid password'}, status_code=401)
+        else:
+            print(error_message)
+            return HTTPException(detail={'message': 'Error updating user'}, status_code=400)
+
 # ping endpoint
 @app.post("/ping", include_in_schema=False)
 async def validate(request: Request):
